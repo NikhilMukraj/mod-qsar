637151ea3306a01b6bbf89890c5e1224da5a215f
Nikhiljag <nikhilmukraj@gmail.com>
2022-12-18 15:00:55 -0500

    tokenization function

--- tokenization.py
+++ tokenization.py
@@ -1,1 +1,13 @@
-# write a function to seperate string into list of tokens
+from SmilesPE.pretokenizer import atomwise_tokenizer
+import pandas as pd
+import os
+
+
+def return_tokens(string):
+    '''
+    Return SMILES tokens of string and if they are in vocabulary
+    '''
+    tokens = atomwise_tokenizer(string)
+    possible_tokens = pd.read_csv(f'{os.getcwd()}\\vocab.csv').iloc[:, 0].to_list()
+    
+    return tokens, any(i not in possible_tokens for i in tokens)
