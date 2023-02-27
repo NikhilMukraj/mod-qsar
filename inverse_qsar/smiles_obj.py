e410132f8d031f1d7505c6f503fcd23caeac5611
Nikhiljag <nikhilmukraj@gmail.com>
2023-02-27 10:13:56 -0500

    inverse_qsar updates

---
+++ inverse_qsar/smiles_obj.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+from tokenization import return_tokens
+import os
+from rdkit import Chem
+from rdkit.Chem import Draw
+import matplotlib.pyplot as plt
+import py3Dmol
+
+
+def renderMol(string, name='Molecule', plot3d=False, save=False):
+    if not plot3d:
+        mol = Chem.MolFromSmiles(string)
+
+        fig = Draw.MolToMPL(mol)
+        plt.title(name)
+        plt.axis('off')
+        if not save:
+            plt.show()
+        else:
+            plt.savefig(f'{os.getcwd()}\\{name}.png')
+    elif plot3d and save:
+        raise NotImplementedError()      
+    else:
+        mblock = Chem.MolToMolBlock(mol)
+
+        view = py3Dmol.view(width=200, height=200)
+        view.addModel(mblock, 'mol')
+        view.setStyle('stick')
+        view.zoomTo()
+        view.show()
+
+class SMILES:
+    def __init__(self, string=None, vocab=None, tokenizer_dict=None, reverse_tokenizer=None):
+        self.string = string
+
+        if self.string is not None:
+            self.tokens, invalidity = return_tokens(string)
+
+            if invalidity:
+                raise Exception("String contains tokens not within vocabulary")
+
+        if vocab is None:
+            self.vocab = pd.read_csv(f'{os.getcwd()}\\vocab.csv').iloc[:, 0].to_list()
+        else:
+            self.vocab = vocab
+
+        self.max_value = len(self.vocab) - 1 # !! important !! needs to be considered before onehot encoding
+        self.bit_length = len(bin(self.max_value)[2:])
+        self.max_bit_value = int('1' * self.bit_length, 2)
+        self.buffer = self.max_bit_value - self.max_value
+
+        if tokenizer_dict is None:
+            self.tokenizer_dict = {i : n for n, i in enumerate(self.vocab)}
+        else:
+            self.tokenizer_dict = tokenizer_dict
+        
+        if reverse_tokenizer is None:
+            self.reverse_tokenizer = {value : key for key, value in self.tokenizer_dict.items()}
+        else:
+            self.reverse_tokenizer = reverse_tokenizer
+
+        self.bit_string = None
+
+    def randomTokens(self, n):
+        self.tokens = np.random.choice(list(self.tokenizer_dict), size=np.random.randint(3, n)).tolist()
+
+        return self.tokens
+
+    def encode(self, tokens=None):
+        if tokens is None:
+            tokens = self.tokens
+
+        bit_string = ''
+
+        for i in tokens:
+            bit_string += str(bin(self.tokenizer_dict[i] + self.buffer)[2:]).zfill(self.bit_length)
+
+        self.bit_string = bit_string
+
+        return bit_string
+
+    def decode(self, encoded_string=None):
+        if encoded_string is None:
+            encoded_string = self.bit_string
+
+        tokens = []
+
+        for i in range(0, len(encoded_string) - self.bit_length + 1, self.bit_length):
+            tokens.append(self.reverse_tokenizer[int(encoded_string[i:i+self.bit_length], 2) - self.buffer])
+
+        return tokens
+
+    def underflowBitString(self, input_string=None):
+        # make sure to check if mutation makes value fall below buffer
+        # if below underflow back to top
+        
+        if input_string is None:
+            input_string = self.bit_string
+
+        strings = []
+
+        for i in range(0, len(input_string) - self.bit_length + 1, self.bit_length):
+            part = int(input_string[i:i+self.bit_length], 2)
+            if part < self.buffer:
+                part = self.max_bit_value - part
+            
+            strings.append(str(bin(part))[2:].zfill(self.bit_length))
+
+        self.bit_string = ''.join(str(i) for i in strings)
