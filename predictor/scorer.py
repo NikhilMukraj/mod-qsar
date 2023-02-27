36cca002659ff740e7207f322edb7438ab1ca929
Nikhiljag <nikhilmukraj@gmail.com>
2023-02-27 10:13:25 -0500

    prediction code testing

---
+++ predictor/scorer.py
@@ -0,0 +1,33 @@
+from sklearn.metrics import log_loss
+import numpy as np
+import pandas as pd
+from c_wrapper import seqOneHot
+
+
+def augmented_predict(model, string, n=5):
+    ############ major problem ##################
+    # [nh] is a token that idk what to do about #
+    # pls find fix                              #
+    #############################################
+    # https://en.wikipedia.org/wiki/Simplified_molecular-input_line-entry_system
+    
+    # use julia func to convert and return seqs
+    # integrate sequence encoding directly into that func
+    raise NotImplementedError('not implemented yet')
+
+def ensemble_predict(string, models_array, vocab=None, tokenizer=None):
+    if vocab is None:
+        vocab = pd.read_csv(f'{os.getcwd()}//vocab.csv')['tokens'].to_list()
+    
+    if tokenizer is None:
+        tokenizer = {i : n for n, i in enumerate(vocab)}
+        
+    initial_seq = np.array([tokenizer[i]+1 for i in return_tokens(test_string, vocab)[0]])
+    full_seq = np.hstack([np.zeros(max_len-len(initial_seq)), initial_seq])
+    full_seq = seqOneHot(np.array(full_seq, dtype=np.int32), seq_shape).reshape(1, *seq_shape)
+    
+    return np.hstack([i.predict(full_seq, verbose=0) for i in models_array])
+
+def get_score(predicted, target):
+    # should maybe inverse, or remove reverse=True from sorted
+    return log_loss(predicted, target)
---
+++ predictor/train_keras_rnn.py
@@ -0,0 +1,76 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Embedding, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow import config
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+# from tokenization import return_tokens
+# from julia import Main
+# import h5py
+# import hdf5plugin
+
+
+print(config.list_physical_devices('GPU'))
+
+# https://stackoverflow.com/questions/41402409/tensorflow-doesnt-seem-to-see-my-gpu
+# sudo cp /usr/lib/python3/dist-packages/tensorflow/libcudnn* /usr/lib/x86_64-linux-gnu/
+
+# f = h5py.File(f'{os.getcwd()}//augmented_data.jld', 'r')
+# X = np.array(f['X'])
+# X = np.transpose(X)
+# Y = np.array(f['Y'])
+# Y = np.transpose(Y)
+
+''' give more stopping options '''
+
+X = np.load(f'{os.getcwd()}//{sys.argv[1]}')
+Y = np.load(f'{os.getcwd()}//{sys.argv[2]}')
+
+n = 60_000
+
+indicies = pd.DataFrame(range(len(X)), columns=['i'])
+indicies = indicies['i'].sample(n=n).to_numpy()
+
+sampled_X = np.zeros((n, X.shape[1], X.shape[2]))
+sampled_Y = np.zeros((n, Y.shape[1]))
+
+for n, i in enumerate(indicies):
+    sampled_X[n] = X[i]
+    sampled_Y[n] = Y[i]
+
+trainX, testX, trainY, testY = train_test_split(X, Y, test_size=.1)
+trainX.shape, testX.shape, trainY.shape, testY.shape
+
+opt = Adam(.0001)
+input_shape = trainX[0].shape
+
+model = Sequential()
+model.add(LSTM(514, input_shape=input_shape, activation="tanh", return_sequences=True, dropout=0.2)) 
+model.add(LSTM(256, activation="tanh", return_sequences=True, dropout=0.2))
+model.add(LSTM(128, activation="tanh", dropout=0.2))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(32, activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(16, activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(2, activation="softmax"))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+history = model.fit(trainX, trainY, batch_size=32, epochs=int(sys.argv[3]), validation_data=(testX, testY), verbose=1)
+
+train_preds = model.predict(trainX)
+test_preds = model.predict(testX)
+
+hist_df = pd.DataFrame(history.history)
+
+hist_df.to_csv(f'{os.getcwd()}//model_history.csv', index=False)
+model.save(f'{os.getcwd()}//rnn_model.h5')
+
+pd.DataFrame(train_preds).to_csv(f'{os.getcwd()}//train_preds.csv', index=False)
+pd.DataFrame(test_preds).to_csv(f'{os.getcwd()}//test_preds.csv', index=False)
