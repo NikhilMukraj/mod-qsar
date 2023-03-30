de229c2498d21de522f5c6beb85543c148295274
Nikhiljag <nikhilmukraj@gmail.com>
2023-03-30 19:41:01 -0400

    fixed crossentropy using binary version for multilabel

--- inverse_qsar/ml_scorer.py
+++ inverse_qsar/ml_scorer.py
@@ -1,12 +1,8 @@
 import numpy as np
 
 
-def get_score(predictions, targets):
-    N = predictions.shape[0]
-    ce = -np.sum(targets * np.log(predictions)) / N
-    if np.isnan(ce):
-        return 0
-    elif ce == np.inf:
-        return 20
-    else:
-        return ce
+def get_score(y_pred, y_true):
+    y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+    term0 = (1 - y_true) * np.log(1 - y_pred + 1e-7)
+    term1 = y_true * np.log(y_pred + 1e-7)
+    return -np.mean(term0 + term1, axis=0)
