5aa22e9e5238a704174b5acbea1b6eaef46cfe67
Nikhiljag <nikhilmukraj@gmail.com>
2023-05-03 13:34:22 -0400

    removed unnecessary comments

--- preprocessor/final_preprocessing.py
+++ preprocessor/final_preprocessing.py
@@ -9,14 +9,11 @@ import os
 f = h5py.File(f'{sys.argv[1]}_aug_unencoded_data.jld')
 unencoded_data = np.transpose(np.array(f['features']))
 
-# check shape first and then run rest of program
-
 seqs_shape = np.array([len(unencoded_data[0]), np.max(unencoded_data)+1], dtype=np.int32)
 unencoded_data = np.array(unencoded_data, dtype=np.int32)
 
 encoded_seqs = np.array([seqOneHot(i, seqs_shape) for i in unencoded_data])
 
-# encoded_seqs = seqsOneHot(unencoded_data, seqs_shape) 
 # need to ensure is of dtype=np.int32 otherwise data is mangled
 
 np.save(f'{sys.argv[1]}_encoded_seqs.npy', encoded_seqs)
