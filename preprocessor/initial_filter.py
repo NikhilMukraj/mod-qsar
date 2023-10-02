a7b21ccf23bf943971d7a62515dbd065b76a0581
Nikhiljag <nikhilmukraj@gmail.com>
2023-10-01 22:52:00 -0400

    fixed initial filter not working with regression dataset

--- preprocessor/initial_filter.py
+++ preprocessor/initial_filter.py
@@ -1,32 +1,53 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from pandas.api.types import is_string_dtype
+from pandas.api.types import is_numeric_dtype
 import numpy as np
 import os
 import sys
 from SmilesPE.pretokenizer import atomwise_tokenizer
 
 
-if len(sys.argv) < 2:
-    print('Too few args')
+NC = '\033[0m'
+RED = '\033[0;31m'
+
+if len(sys.argv) < 3:
+    print(f'{RED}Too few args{NC}')
     sys.exit()
 
 df = pd.read_csv(f'{sys.argv[1]}', low_memory=False)
 
 df = df[['SMILES', 'ACTIVITY']]
 
-df = df.loc[df['ACTIVITY'] != 'Inconclusive', :]
-df = df[~df['SMILES'].isnull()]
+# check type, if numeric skip this and say youre skipping this
+# if string proceed as normal
+# if neither specify that this is an unknown datatype
 
-activeDF = df.loc[df['ACTIVITY'] == 'Active', :]
-inactiveDF = df.loc[df['ACTIVITY'] == 'Inactive', :]
+# https://stackoverflow.com/questions/19900202/how-to-determine-whether-a-column-variable-is-numeric-or-not-in-pandas-numpy
 
-if len(activeDF) <= len(inactiveDF):
-    filtered = pd.concat([activeDF.loc[:, 'SMILES':'ACTIVITY'], 
-                          inactiveDF.sample(n=len(activeDF)).loc[:, 'SMILES':'ACTIVITY']])
+if is_string_dtype(df['ACTIVITY']):
+    df = df.loc[df['ACTIVITY'] != 'Inconclusive', :]
+    df = df[~df['SMILES'].isnull()]
+
+    activeDF = df.loc[df['ACTIVITY'] == 'Active', :]
+    inactiveDF = df.loc[df['ACTIVITY'] == 'Inactive', :]
+
+    if len(activeDF) <= len(inactiveDF):
+        filtered = pd.concat([activeDF.loc[:, 'SMILES':'ACTIVITY'], 
+                            inactiveDF.sample(n=len(activeDF)).loc[:, 'SMILES':'ACTIVITY']])
+    else:
+        filtered = pd.concat([activeDF.sample(n=len(inactiveDF)).loc[:, 'SMILES':'ACTIVITY'], 
+                            inactiveDF.loc[:, 'SMILES':'ACTIVITY']])
+
+    filtered.to_csv(f'{sys.argv[2]}_filtered_dataset.csv', index=False)  
+elif is_numeric_dtype(df['ACTIVITY']):
+    print('Numeric dataset found, skipping boolean based filtration')
+
+    df = df[~df['SMILES'].isnull()]
+
+    df.to_csv(f'{sys.argv[2]}_filtered_dataset.csv', index=False)  
 else:
-    filtered = pd.concat([activeDF.sample(n=len(inactiveDF)).loc[:, 'SMILES':'ACTIVITY'], 
-                          inactiveDF.loc[:, 'SMILES':'ACTIVITY']])
-
-filtered.to_csv(f'{sys.argv[2]}_filtered_dataset.csv', index=False)
+    print(f'{RED}Dataset of unknown type found, must either be all strings of "Active" or "Inactive" or all numeric values{NC}')
+    sys.exit(1)
 
 print('Filtered dataset created')
