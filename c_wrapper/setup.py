84f3ad3435be6b14ec62227cabc25044fb0541ad
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-01 18:44:15 -0500

    unnecessary comments

--- c_wrapper/setup.py
+++ c_wrapper/setup.py
@@ -1,15 +1,8 @@
-# https://stackoverflow.com/questions/45121352/how-to-include-a-shared-c-library-in-a-python-package
-# https://python-packaging.readthedocs.io/en/latest/minimal.html
 from setuptools import setup
   
 
-# specify requirements of your package here
 requirements = ['numpy']
 
-# packages=['tokenization', 'smienumeration'],
-# package_dir={"":"smiles_tools"},
-
-# calling the setup function 
 setup(name='c_wrapper',
     version='0.1.0',
     description='Some helper functions written in C',
