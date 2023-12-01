e92495505a9232d20e5179b163731da5bf5f8a1a
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-01 18:44:21 -0500

    unnecessary comments

--- smiles_tools/setup.py
+++ smiles_tools/setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup
   
 
-# specify requirements of your package here
 requirements = ['numpy', 'pandas', 'rdkit']
 
-# packages=['tokenization', 'smienumeration'],
-# package_dir={"":"smiles_tools"},
-
-# calling the setup function 
 setup(name='smiles_tools',
     version='0.1.1',
     description='Some simple tools for SMILES strings',
