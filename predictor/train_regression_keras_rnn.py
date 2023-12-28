7555d684c0c089d9d91d9163383a31864b3597bf
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-27 22:58:48 -0500

    cutoff binding affinity at max in regression

--- predictor/train_regression_keras_rnn.py
+++ predictor/train_regression_keras_rnn.py
@@ -24,8 +24,8 @@ trainX, testX, trainY, testY = train_test_split(X, Y, test_size=.1)
 print(trainX.shape, testX.shape, trainY.shape, testY.shape)
 
 # maybe input with sys.argv
-trainX[trainX > 500] = 500
-testX[testX > 500] = 500
+trainY[trainY > 500] = 500
+testY[testY > 500] = 500
 
 opt = Adam(.0001)
 input_shape = trainX[0].shape
